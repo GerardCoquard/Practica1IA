065783ca7db417fe1862b088878e21d1f52c0813
GerardCoquard <gerard.coquard.franco@gmail.com>
2023-02-16 20:02:50 +0100

    dfsdsgdf puta mierda

--- Assets/Prac_01/Scripts/Food.cs
+++ Assets/Prac_01/Scripts/Food.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 namespace Steerings {
     public class Food : MonoBehaviour
     {
+        public float radius;
+        bool quitting;
         public FoodSpawner mySpawner;
+        public LayerMask whatIsFood;
         private void OnDisable() {
-            if(!mySpawner.Equals(null))
+            if(!mySpawner.Equals(null) && !quitting)
             {
-                FoodSpawner.SetPosition(gameObject,mySpawner);
-                transform.SetParent(mySpawner.transform);
+                StartCoroutine(FoodSpawner.SetPosition(gameObject,mySpawner,radius,whatIsFood));
             }
+        }
+        private void OnApplicationQuit() {
+            quitting = true;
         }
     }
 }
--- Assets/Prac_01/Scripts/FoodSpawner.cs
+++ Assets/Prac_01/Scripts/FoodSpawner.cs
@@ -8,38 +8,56 @@ namespace Steerings {
         public float radius;
         public int amountOfFood;
         public GameObject prefab;
-        public LayerMask whatIsFood;
+        static List<Vector3> points = new List<Vector3>();
+        static List<float> radios = new List<float>();
+        private void OnDrawGizmos() {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position,radius);
+            Gizmos.color = Color.blue;
+            for (int i = 0; i < points.Count; i++)
+            {
+                Gizmos.DrawWireSphere(points[i],radios[i]);
+            }
+        }
         private void Start() {
             for (int i = 0; i < amountOfFood; i++)
             {
-                GameObject f = Instantiate(prefab,transform);
-                f.GetComponent<Food>().mySpawner = this;
-                FoodSpawner.SetPosition(f,this);
+                GameObject f = Instantiate(prefab);
+                Food food = f.GetComponent<Food>();
+                food.mySpawner = this;
+                StartCoroutine(FoodSpawner.SetPosition(f,this,food.radius,food.whatIsFood));
             }
         }
-        public static void SetPosition(GameObject target, FoodSpawner spawner)
+        static bool Overlapping(Vector3 point, float _radius, LayerMask foodLayer)
         {
+            points.Add(point);
+            radios.Add(_radius);
+            Collider[] col = Physics.OverlapSphere(point,_radius, foodLayer);
+            foreach (var item in col)
+            {
+                Debug.Log(item.name);
+            }
+            return col.Length > 0;
+        }
+        public static IEnumerator SetPosition(GameObject target, FoodSpawner spawner, float _radius, LayerMask foodLayer)
+        {
+            bool firstFrame = false;
+            while(firstFrame) {firstFrame = false; yield return null;}
+            int n = 0;
             int angle = Random.Range(0,361);
             float distance = Random.Range(0f,spawner.radius);
-            Vector3 desiredPosition =  Utils.OrientationToVector(angle).normalized * distance + spawner.transform.position;
-            while(Overlapping(desiredPosition))
+            Vector3 desiredPosition =  (Utils.OrientationToVector(angle).normalized * distance) + spawner.transform.position;
+            while(Overlapping(desiredPosition,_radius,foodLayer))
             {
+                n++;
+                Debug.Log(n);
                 angle = Random.Range(0,361);
                 distance = Random.Range(0f,spawner.radius);
                 desiredPosition =  Utils.OrientationToVector(angle).normalized * distance + spawner.transform.position;
             }
-            if(spawner.gameObject.activeSelf) spawner.StartCoroutine(spawner.SetActive(target));
-        }
-        static bool Overlapping(Vector3 point)
-        {
-            Vector3 cameraPos = Camera.main.gameObject.transform.position;
-            Vector3 direction = point - cameraPos;
-            return Physics.Raycast(cameraPos,direction,Mathf.Infinity, LayerMask.NameToLayer("Food"));
-        }
-        IEnumerator SetActive(GameObject target)
-        {
-            yield return new WaitForEndOfFrame();
+            target.transform.position = desiredPosition;
             target.SetActive(true);
+            target.transform.SetParent(spawner.transform);
         }
     }
 }
